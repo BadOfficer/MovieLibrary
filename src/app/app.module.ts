fd73bd07c4b8658dcee79b4299ae845e1ebf6433
Taras <tarasbondarenko2005@gmail.com>
2024-03-05 23:03:50 +0200

    Added connection to database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from 'src/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import configuration from 'src/configuration';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot({
+  imports: [UsersModule, SequelizeModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      dialect: "postgres",
+      host: configService.get("db_host"),
+      port: configService.get("db_port"),
+      username: configService.get("db_user"),
+      database: configService.get("db_name"),
+      password: configService.get("db_password"),
+      models: [],
+      synchronize: true,
+      autoLoadModels: true
+    })
+  }), ConfigModule.forRoot({
     isGlobal: true,
     load: [configuration]
   })],
