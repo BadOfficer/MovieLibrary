dc800dcbc4a2b0caf26947edfccaae636d9c27cd
Taras <tarasbondarenko2005@gmail.com>
2024-03-06 13:46:47 +0200

    Added API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get("port");
+  const conifg = new DocumentBuilder().setTitle("movie-library API").setDescription("API for movie-library").setVersion("1.0").addTag("API").build()
+  const document = SwaggerModule.createDocument(app, conifg)
+  SwaggerModule.setup("api", app, document)
   await app.listen(port);
 }
 bootstrap();
