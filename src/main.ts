be3117b55b3800939d6ce8830ce6170f831e8fe6
Taras <tarasbondarenko2005@gmail.com>
2024-03-06 16:07:53 +0200

    Added user models & DTO

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get("port");
+  app.useGlobalPipes(new ValidationPipe())
   const conifg = new DocumentBuilder().setTitle("movie-library API").setDescription("API for movie-library").setVersion("1.0").addTag("API").build()
   const document = SwaggerModule.createDocument(app, conifg)
   SwaggerModule.setup("api", app, document)
