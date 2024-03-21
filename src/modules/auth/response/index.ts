6fa5129314e43fefbae8445a08fc64d232a9355e
Taras Bondarenko <tarasbondarenko2005@gmail.com>
2024-03-21 11:52:31 +0200

    Added Auth into API documentation

--- src/modules/auth/response/index.ts
+++ src/modules/auth/response/index.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsString } from "class-validator"
 
 export class AuthUserResponse {
+    @ApiProperty()
     @IsString()
     first_name: string
 
+    @ApiProperty()
     @IsString()
     last_name: string
 
+    @ApiProperty()
     @IsString()
     email: string
 
+    @ApiProperty()
     @IsString()
     password: string
 
+    @ApiProperty()
     @IsString()
     token: string
 }
--- src/modules/users/DTO/index.ts
+++ src/modules/users/DTO/index.ts
@@ -1,15 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class CreateUserDTO{
+    @ApiProperty()
     @IsString()
     first_name: string
 
+    @ApiProperty()
     @IsString()
     last_name: string
 
+    @ApiProperty()
     @IsString()
     password: string
 
+    @ApiProperty()
     @IsString()
     email: string
 }
